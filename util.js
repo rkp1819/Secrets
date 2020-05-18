9ee16380796c06abfd50a053b7879f3dbf31c135
rkp1819 <rkp1819@gmail.com>
2020-05-18 13:54:20 +0530

    bcrypt encryption

--- util.js
+++ util.js
@@ -1,31 +1,44 @@
-md5 = require('md5');
+// md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 exports.saveUser = function(req, res, User){
-  user = new User({
-    email:req.body.email,
-    password:md5(req.body.password),
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      // Store hash in your password DB.
+      user = new User({
+        email:req.body.email,
+        password:hash,
+      });
+      User.create(user, function(err, doc){
+          if (err) {
+            console.log(err);
+            res.status(500).send(e);
+          } else {
+            console.log('Saved User: ',req.body.email, hash);
+          }
+      });
   });
-  User.create(user, function(err, doc){
-      if (err) {
-        console.log(err);
-        res.status(500).send(e);
-      } else {
-        console.log('Saved User: ',req.body.email, md5(req.body.password));
-      }
-  });
+
+
 };
 
 exports.getUser = function(req, res, User){
-  User.findOne({ email: req.body.email }, function(err, founduser){
-    if (err) {
-      console.log('error in getUser findOne: ',err);
-    } else {
-      if(null==founduser || founduser.password!=md5(req.body.password)){
-        console.log('Authentication error: ',req.body.email);
-        res.render('login', {errors: ['Authentication Error, Please try again']});
-      }else{
-        console.log('Logged In! ',req.body.email)
-        res.render('secrets');
-      }
-    }
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      User.findOne({ email: req.body.email }, function(err, founduser){
+        if (err) {
+          console.log('error in getUser findOne: ',err);
+        } else {
+          bcrypt.compare(req.body.password, hash, function(err, result) {
+            if(!result){
+              console.log('Authentication error: ',req.body.email);
+              res.render('login', {errors: ['Authentication Error, Please try again']});
+            }else{
+              console.log('Logged In! ',req.body.email)
+              res.render('secrets');
+            }
+          });
+        }
+      });
   });
+
 };
