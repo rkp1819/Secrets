5e8dbd87a72d3c6a0a3ba8776c3461786b7a8cba
rkp1819 <rkp1819@gmail.com>
2020-05-24 10:43:51 +0530

    passport-local-mongoose

--- app.js
+++ app.js
@@ -7,33 +7,64 @@ const md5 = require('md5');
 const ejs = require('ejs');
 const app = express();
 const util = require(__dirname+'/util.js');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('passport-local')
+const passportLocalMongoose = require('passport-local-mongoose')
+
+mongoose.set('useCreateIndex', true);
+console.log("Environment: ",app.get('env'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 // const encrypt = require('mongoose-encryption');
 
+//session stuff
+const sess = {
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {}
+}
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
+}
+app.use(session(sess))
+
+//passport stuff
+app.use(passport.initialize());
+app.use(passport.session());
+
  //db
  mongoose.connect("mongodb://localhost:27017/userDB", { useUnifiedTopology: true, useNewUrlParser: true});
 
  const userSchema = new mongoose.Schema({
-     email: {
+     username: {
        type: String,
-       required: [true, "use a email id"]
+       // required: [true, "use a username id"]
      },
      password: {
        type: String,
-       required: [true, "use a password"]
+       // required: [true, "use a password"]
      }
  });
 
 // const encKey = process.env.SOME_32BYTE_BASE64_STRING;
 // const sigKey = process.env.SOME_64BYTE_BASE64_STRING;
-
 // userSchema.plugin(encrypt, { encryptionKey: encKey, signingKey: sigKey, encryptedFields: ['password'] });
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model('User', userSchema);
 
+//passportLocalMongoose stuff
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 
@@ -42,24 +73,63 @@ app.get('/', function(req, res){
 });
 
 app.get('/register', function(req, res){
+  console.log("---------------register get route-----------------");
   res.render('register');
 });
 
 app.post('/register', function(req, res){
-  util.saveUser(req, res, User);
-  res.render('secrets');
+  // util.saveUser(req, res, User);
+  // res.render('secrets');
+  console.log("------------Register post route------------------");
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect('/secrets');
+      });
+    }
+  });
 });
 
 app.get('/login', function(req, res){
+  console.log("---------------login get route--------------------");
   res.render('login', {errors: []});
 })
 
 app.post('/login', function(req, res){
-  util.getUser(req, res, User)
+  console.log("-------------------login post route---------------------");
+  // util.getUser(req, res, User);
+  console.log(req.body);
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+      passport.authenticate('local', function(err, user, info) {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          console.log('user: ', user, 'err: ', err, 'info', info);
+          return res.render('login', {errors: [info.message]});
+        }
+        req.logIn(user, function(err) {
+          if (err) {
+            return next(err);
+          }
+          return res.redirect('secrets');
+        });
+      })(req, res, next=function(err){
+        console.log('err: ', err);
+        res.render('login', {errors: [err]})
+      });
 });
 
 app.get('/logout', function(req, res){
-  res.render('home');
+  console.log("------------------logout get route------------------");
+  req.logout();
+  res.redirect('/');
 });
 
 app.get('/submit', function(req, res){
@@ -67,58 +137,20 @@ app.get('/submit', function(req, res){
 });
 
 
-
-
-
-
-
-
-
-
-//post specific
-app.post("/users", function(req, res){
-  util.saveUser(req, res, User);
+app.get('/secrets', function(req, res){
+  console.log("-------------------Secrets get route---------------------");
+  if(req.isAuthenticated()){
+    res.render('secrets');
+  }else{
+    res.redirect('/login');
+  }
 });
 
-//get specific
-app.get("/users/:id", function(req, res){
-  util.getUser(req, res, User);
-});
 
-//replace specific
-app.put("/users/:id", function(req, res){
-  user = new User({ email: req.body.email, password: req.body.password });
-  User.replaceOne({ _id: req.params.id }, { email: req.body.email, password: req.body.password }, function(err, writeOpResult){
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      console.log(writeOpResult);
-      res.sendStatus(200);
-    }
-  });
-});
 
-//update specific
-app.patch("/users/:id", function(req, res){
-  User.updateOne({ _id: req.params.id }, { password: req.body.password }, function(err){
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.sendStatus(200);
-    }
-  });
-});
 
-//delete specific
-app.delete("/users/:id", function(req, res){
-  User.deleteOne({ _id: req.params.id }, function(err){
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.sendStatus(200);
-    }
-  });
-});
+
+
 
 
 
